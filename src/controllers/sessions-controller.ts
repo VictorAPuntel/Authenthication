609a87f57c170d788b8c8b7b5c23a7c02c3d0440
VictorAPuntel <victor.a.puntel@gmail.com>
2025-08-11 00:29:59 -0300

    Fazendo verificação de usuario e senha para logar

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,8 +1,28 @@
+import { AppError } from '@/utils/AppError'
 import { Request, Response } from 'express'
-
+import { authConfig } from '@/configs/auth'
+import { sign } from 'jsonwebtoken'
 class SessionsController {
   async create(request: Request, response: Response) {
-    return response.json({ message: 'ok' })
+    const { username, password } = request.body
+
+    const fakeUser = {
+      id: 1,
+      username: 'victor',
+      password: '123456',
+    }
+
+    if (username !== fakeUser.username || password !== fakeUser.password) {
+      throw new AppError('Usuário e/ou senha incorreto', 401)
+    }
+
+    const { secret, expiresIn } = authConfig.jwt
+    const token = sign({}, secret, {
+      expiresIn,
+      subject: String(fakeUser.id),
+    })
+
+    return response.json({ token })
   }
 }
 
