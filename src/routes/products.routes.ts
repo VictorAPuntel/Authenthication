f64abd2da23c9add273be234ac8efa5c15dcaf19
VictorAPuntel <victor.a.puntel@gmail.com>
2025-08-12 00:30:00 -0300

    Aplicando autorização na rota de create products

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,11 +1,19 @@
 import { Router } from 'express'
 import { ProductsController } from '@/controllers/products-controller'
 import { ensureAuthenticated } from '@/middlewares/ensureAuthenticated'
+import { verifyUserAuthorization } from '@/middlewares/verify-userAuthorization'
 
 const productsRoutes = Router()
 const productsController = new ProductsController()
 
+//Aplicar autorização em todas as rotas abaixo
+// productsRoutes.use(verifyUserAuthorization(['sale', 'admin']))
 productsRoutes.get('/', productsController.index)
-productsRoutes.post('/', ensureAuthenticated, productsController.create)
+productsRoutes.post(
+  '/',
+  ensureAuthenticated,
+  verifyUserAuthorization(['sale', 'admin']),
+  productsController.create
+)
 
 export { productsRoutes }
